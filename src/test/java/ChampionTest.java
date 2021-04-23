6e9d82b1f998bc715bad3def54c2e4380235b2b3
Hyoeun Kim <gydms1355@naver.com>
2021-04-23 22:31:28 +0900

    1week

--- src/test/java/ChampionTest.java
+++ src/test/java/ChampionTest.java
@@ -4,6 +4,11 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class ChampionTest {
     private List<Champion> championList = new ArrayList<Champion>();
@@ -30,18 +35,22 @@ public class ChampionTest {
     @Test
     public void givenCollectionWhenEmptyCorrect() {
         List<String> emptyList = new ArrayList<>();
+        assertTrue(emptyList.isEmpty());
+        assertThat(emptyList, empty());
     }
 
     //notNullValue 활용한 테스트
     @Test
     public void notNullCheck() {
         String lck = "LCK";
+        assertThat(lck, notNullValue());
     }
 
     //nullValue 활용한 테스트
     @Test
     public void givenStringWhenNullIsCorrect() {
         String lck = null;
+        assertThat(lck, nullValue());
     }
 
     //문자열 관련 테스트 anyOf, containsString, endWith
@@ -51,44 +60,75 @@ public class ChampionTest {
         String sampleString2 = "Player point";
         String startString = "Player";
         String endString = "point";
+        // anyof는 값이 하나라도 맞으면 pass(조건 여러개)
+        // allof는 값이 모두 맞아야 pass
+        //assertThat(sampleString1, anyOf(notNullValue(), containsString("Focus")));
+        //assertThat(sampleString2, containsString(("Player point")));
+        //assertThat(startString, endsWith("r"));
+
+        // anyof 안에 포함된 조건들 중 하나라도 pass하면 ok
+        //assertThat(sampleString1, startsWith("Player"));
+        //assertThat(sampleString2, endsWith("point"));
+        //assertThat(sampleString1, containsString("Fo"));
+
+        // 위의 세가지를 묶어서
+        assertThat(sampleString1, anyOf(startsWith("Player"), endsWith("Focus")));
+        assertThat(sampleString2, allOf(startsWith("Player"), endsWith("point"), containsString("11")));
     }
 
     //부동소수점 범위 closeTo 테스트
     @Test
     public void testForFloatingPoint() {
+        assertThat(3.14, closeTo(3, 0.2));
+        // 3에서 오차범위는 0.2
 
+        assertThat(3.14, closeTo(3, 0.1));
+        // 오차범위 0.1
     }
 
     //anything 테스트
     @Test
     public void shouldNotErrorGetReference() {
-
+        System.out.println("champion info :: " + championList.get(0));
+        assertThat(championList.get(0), anything());
+        // 챔피언리스트0의 값이 무엇인지
     }
 
     //객체 크기 검증 테스트 hasSize
     @Test
     public void shouldChampionCountFive() {
-
+        assertThat(championList, hasSize(5));
+        assertThat(championList.size(), is(5));
     }
 
-    //서폿 챔피언은 타릭이어야 한다라는 조건으로 테스트 코드 작성
+    //타릭 챔피언 객체를 만들고 이름과 포지션을 검증하세요
     @Test
     public void shouldSupportChampionIsTaric() {
         Champion supportChamp = new Champion("타릭", "바텀");
-
+        assertThat("타릭", is(supportChamp.getName()));
+        assertThat("타릭", equalTo(supportChamp.getName()));
+        assertThat("서폿", equalTo(supportChamp.getPosition()));
     }
 
     //hasProperty 활용하여 속성이 포함되어 있는지 테스트
     @Test
     public void shouldHasPropertyPosition() {
-
+        //assertThat(championList.get(0), hasProperty("posistion"));
+        assertThat(championList.get(1), hasProperty("name"));
+        assertThat(championList.get(2), hasProperty("position", equalTo(("미드"))));
+        System.out.println("champion :: " + championList.get(2));
+        // 객체 내에 property가 있는지 확인
     }
 
     //hasToString 활용 테스트
     @Test
     public void shouldHaveSomeChampName() {
         List<String> champListNames = Arrays.asList("루시안", "애쉬", "렉사이", "갈리오", "모르가느", "블라디미르");
+        assertThat(champListNames.get(0), hasToString("루시안"));
+        // champListNames배열의 인덱스0에 루시안이 있는지 확인한다.
 
+        assertThat(champListNames.get(0), containsString("루시"));
+        // 인덱스0에 해당 문자열에 포함되어 있는지만 확인한다.
     }
 
     //property와 value가 같은지 테스트
@@ -96,13 +136,20 @@ public class ChampionTest {
     public void shouldHaveSamePropertyAndValue() {
         List<String> championNames1 = Arrays.asList("루시안", "애쉬", "렉사이", "갈리오", "모르가나", "블라디미르");
         List<String> championNames2 = Arrays.asList("루시안", "애쉬", "렉사이", "갈리오", "모르가나", "블라디미르");
-
+        List<String> champArray = championNames1;
+        assertThat(championNames1, samePropertyValuesAs(championNames2));
+        assertThat(championNames1, samePropertyValuesAs(champArray));
     }
 
     //탑 챔피언은 다리우스여야 한다라는 조건으로 테스트 코드 작성, stream 활용예
     @Test
     public void shouldTopChampionIsDarius() {
+        Optional<Champion> filteredChampion = championList.stream()
+                .filter(c -> c.getPosition().equals("탑"))
+                .findFirst();
 
+        assertThat(filteredChampion.get().getName(), is("다리우스"));
+        System.out.println("champ :: " + filteredChampion);
     }
 
 }
--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -15,6 +15,8 @@ public class CustomCalculatorTest {
         customCalculator = new CustomCalculator();
         int result = customCalculator.add(10,15);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result == 25);
+        assertFalse(result == 24);
         System.out.println("result :: " + result);
     }
 
@@ -24,6 +26,7 @@ public class CustomCalculatorTest {
         customCalculator = new CustomCalculator();
         int result = customCalculator.subtract(23,10);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result == 13);
         System.out.println("result :: " + result);
     }
 
@@ -33,6 +36,8 @@ public class CustomCalculatorTest {
         customCalculator = new CustomCalculator();
         int result = customCalculator.multiply(5,9);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result == 45);
+        //assertTrue(customCalculator.multiply(2,3) == 6);
         System.out.println("result :: " + result);
     }
 
@@ -42,6 +47,8 @@ public class CustomCalculatorTest {
         customCalculator = new CustomCalculator();
         int result = customCalculator.divide(25,5);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result == 5);
+        //assertTrue(customCalculator.divide(25,5) == 5);
         System.out.println("result :: " + result);
     }
 }
